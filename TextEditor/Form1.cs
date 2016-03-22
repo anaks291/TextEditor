8c8ed2703fa6a9e3f5d31f9d5ddc31bc6cd4d7fa
anaks291 <anaks291@gmail.com>
2016-03-22 16:45:24 +0300

    Revert "MainForm is designed"
    This reverts commit 0c579e8b92b2bb25996fbfc496cde1c0f193eaca.

--- TextEditor/Form1.cs
+++ TextEditor/Form1.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace TextEditor
 {
-    public partial class MainForm : Form
+    public partial class Form1 : Form
     {
-        public MainForm()
+        public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
